f2eafe59555ff55983103861d79ea680620e1e0b
returnzork <hellowatex@gmail.com>
2012-11-12 12:43:31 -0800

    First commit

---
+++ BackupV3 API/API.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.Windows.Forms;
+
+namespace BackupV3_API
+{
+    public interface BackupV3API
+    {
+        void Work(string[] Imports);
+
+        void Interface();
+
+        string Author();
+        string Version();
+        string Name();
+
+    }
+}
